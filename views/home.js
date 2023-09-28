6661f63b06e19a9ab92bf8c12599e1e12a3219c7
Anunciada <jamilly.d.castro@accenture.com>
2023-09-28 19:24:10 -0300

    tela de home/escolha de loja

--- views/home.js
+++ views/home.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
 import React from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView, View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import Footer from '../footer'
 
 export default function Home({ navigation }) {
   return (
@@ -22,7 +22,7 @@ export default function Home({ navigation }) {
 
         <TouchableOpacity>
           <Image
-            style={styles.topPanelIcon}
+            style={styles.menuIcon}
             source={{
               uri: 'https://api.iconify.design/material-symbols:top-panel-open-outline-rounded.svg',
             }}
@@ -30,16 +30,38 @@ export default function Home({ navigation }) {
         </TouchableOpacity>
       </View>
 
-      <View style={styles.body}>
-        <View style={styles.card}>
+      <ScrollView style={styles.body}>
+        <View style={styles.anuncio}>
           <Image
-            style={styles.cardImage}
+            style={styles.anuncioImage}
             source={require('/views/img/group5979.png')}
           />
         </View>
-        <Text style={styles.title}>Refeições</Text>
-      </View>
+        <Text style={styles.title}>Restaurantes</Text>
+
+        <View style={styles.cards}>
+          {[1, 2, 3, 4, 5].map((_, index) => (
+            <TouchableOpacity key={index} style={styles.card}>
+              <View>
+                <Image
+                  style={styles.logoLoja}
+                  source={require('/views/img/logoLoja.png')}
+                />
+              </View>
+              <View>
+                <Text style={styles.nomeLoja}>Boi e Brasa Spettus</Text>
+                <View style={styles.infoLoja}>
+                  <Text style={styles.infoLojaTime}>70 min        </Text>
+                  <Text style={styles.infoLojaStatus}>Aberto</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </ScrollView>
+      <Footer />
     </View>
+    
   );
 }
 
@@ -47,49 +69,87 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'flex-end',
     backgroundColor: '#000A0F',
   },
   header: {
-    flex: 1,
     flexDirection: 'row',
     backgroundColor: '#001119',
     alignItems: 'center',
     justifyContent: 'space-between',
-    paddingHorizontal: 20,
   },
   menuIcon: {
     width: 25,
     height: 25,
     tintColor: '#ffffff',
+    margin: 20,
   },
   logo: {
     width: '30%',
     height: '75%',
-    margin: 80,
-  },
-  topPanelIcon: {
-    width: 20,
-    height: 23,
-    tintColor: '#ffffff',
   },
   body: {
-    flex: 8,
-  },
-  card: {
-    alignItems: 'center',
-  },
-  cardImage: {
-    width: 340,
-    height: 130,
-    marginTop: 30,
-    marginBottom: 50,
+    flex: 1,
   },
   title: {
     color: '#E1E1E6',
     fontSize: 18,
     letterSpacing: 1.5,
-    fontWeight: 450,
+    fontWeight: '450',
     marginLeft: 30,
+    marginBottom: 20,
+  },
+  anuncio: {
+    alignItems: 'center',
+  },
+  anuncioImage: {
+    width: 340,
+    height: 130,
+    marginTop: 30,
+    marginBottom: 50,
+  },
+  cards: {
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  card: {
+    width: '85%',
+    backgroundColor: '#00070A',
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 15,
+    marginBottom: 7,
+    flexDirection: 'row',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.4,
+    shadowRadius: 10,
+  },
+  logoLoja: {
+    width: 50,
+    height: 50,
+    marginRight: 20,
+    marginLeft: 10,
+  },
+  infoLoja: {
+    flexDirection: 'row',
+    marginTop: 7,
+  },
+  infoLojaTime: {
+    color: '#E1E1E6',
+    fontSize: 14,
+    fontWeight: '350',
+  },
+  infoLojaStatus: {
+    color: '#82F3FF',
+    fontSize: 14,
+    fontWeight: '450',
+  },
+  nomeLoja: {
+    color: '#E1E1E6',
+    fontSize: 14,
+    fontWeight: '350',
   },
 });
