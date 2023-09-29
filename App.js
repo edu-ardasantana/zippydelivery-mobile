18b80172fd88e8203e2336d649111c02753ca050
Eduarda Santana <edu.ardasantana@outlook.com>
2023-09-29 17:41:05 -0300

    add telas de login e cadastro

--- App.js
+++ App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './views/home';
+import Login from './views/login';
+import RecuperaSenha from './views/recuperaSenha';
+import CadastraUsuario from './views/cadastraUsuario';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator initialRouteName='Login'>
         <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
+        <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
+        <Stack.Screen name="RecuperaSenha" component={RecuperaSenha} options={{headerShown: false}} />
+        <Stack.Screen name="CadastraUsuario" component={CadastraUsuario} options={{headerShown: false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
