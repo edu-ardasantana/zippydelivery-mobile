4aa38261b2e9933c98f2658abf5581dff0e05534
Jamilly Anunciada <jamillymilly10@hotmail.com>
2023-09-30 10:40:38 -0300

    Update homeLoja.js

--- views/homeLoja.js
+++ views/homeLoja.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { ScrollView, View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
-import Footer from '../footer'
+import { ScrollView, View, Text, TouchableOpacity, Image, StyleSheet, ImageBackground } from 'react-native';
+
 
 export default function HomeLoja({ navigation }) {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
+
+      <ImageBackground style={styles.header} source={{ uri: 'https://imagens1.ne10.uol.com.br/blogsne10/social1/uploads/2014/03/boie-e-brasa.jpg' }} >
+
         <TouchableOpacity>
           <Image
             style={styles.menuIcon}
@@ -14,14 +16,34 @@ export default function HomeLoja({ navigation }) {
             }}
           />
         </TouchableOpacity>
-      </View>
+      </ImageBackground>
 
+      <ScrollView >
+        <View style={styles.bodyContainer}>
+          <ScrollView
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={true}
+            contentContainerStyle={styles.carouselContainer}
+            style={styles.scrollView} // Estilo adicional
+          >
+            {/* Conteúdo do carrossel */}
+            <View style={styles.slide}>
+              <Text>Slide 1</Text>
+            </View>
+            <View style={styles.slide}>
+              <Text>Slide 2</Text>
+            </View>
+            <View style={styles.slide}>
+              <Text>Slide 3</Text>
+            </View>
+          </ScrollView>
 
-      <Footer />
+        </View>
+      </ScrollView>
     </View>
-    
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -30,15 +52,50 @@ const styles = StyleSheet.create({
     backgroundColor: '#000A0F',
   },
   header: {
+    height: 80,
     flexDirection: 'row',
-    backgroundColor: '#001119',
     alignItems: 'center',
     justifyContent: 'space-between',
+    padding: 20,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    overflow: 'hidden',
   },
   menuIcon: {
     width: 25,
     height: 25,
-    tintColor: '#ffffff',
-    margin: 20,
+    tintColor: '#000',
+    borderRadius: 50,
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+  },
+  bodyContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  scrollView: {
+    width: 300, // Defina a largura do ScrollView
+    height: 200, // Defina a altura do ScrollView
+    backgroundColor: 'lightgray', // Cor de fundo para visualização
+    borderWidth: 2, // Adicione bordas para visualização
+    borderColor: 'gray', // Cor das bordas
+  },
+  carouselContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  slide: {
+    width: 300, // Largura de cada slide
+    height: 200, // Altura de cada slide
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ECECEC',
+    marginHorizontal: 10, // Espaçamento horizontal entre os slides
+    borderRadius: 10, // Borda arredondada para os slides
   },
 });
