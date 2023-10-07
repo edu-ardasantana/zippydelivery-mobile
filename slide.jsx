a7c67317d05064e59f028b46e504c5d5dd879681
Anunciada <jamilly.d.castro@accenture.com>
2023-10-07 01:11:23 -0300

    impl mudanças de layout e cores na tela da loja

--- slide.jsx
+++ slide.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
-import { Button } from 'react-native-elements';
 
 export default function Slide() {
 
     const [quantity, setQuantity] = useState(1);
-    const [isFavorite, setIsFavorite] = useState(false);
 
     function incrementQuantity() { setQuantity(quantity + 1) }
     function decrementQuantity() { quantity > 0 && setQuantity(quantity - 1) }
@@ -16,28 +14,22 @@ export default function Slide() {
             : '';
     }
 
-    function flagFavorite() { setIsFavorite(!isFavorite) };
-
-    const heartImageSource = isFavorite
-    ? { uri: 'https://api.iconify.design/material-symbols:favorite-rounded.svg' }
-    : { uri: 'https://api.iconify.design/material-symbols:favorite-outline-rounded.svg' };
-
     return (
-        <View style={styles.slide}>
-            <View style={styles.line1}>
+        <TouchableOpacity style={styles.slide}>
+            <View style={styles.colum1}> {/* nome do prato, breve descrição, preço, alinhado em baixo do outro */}
+                <Text style={styles.nomeItem}>Salada Ravanello </Text>
+                <Text style={styles.descricao}>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim.</Text>
+                <Text style={[styles.title2, { color: '#FF9431' }]}>{formatarMoeda(44.90)}</Text>
+
+            </View>
+
+            <View style={styles.colum2}> {/* imagem do prato */}
                 <Image style={styles.itemImage} source={require('/views/img/item.png')} />
-                <TouchableOpacity style={styles.heartButton} onPress={flagFavorite}>
-                    <Image style={[styles.icon, { margin: 8 }]} source={heartImageSource} />
-                </TouchableOpacity>
+                
+
             </View>
 
-            <TouchableOpacity style={styles.line2}>
-                <Text style={styles.title3}>Salada Ravanello {'>'}</Text>
-            </TouchableOpacity>
-
-            <View style={styles.line2}>
-                <Text style={[styles.title2, { color: '#82F3FF' }]}>{formatarMoeda(44.90)}</Text>
-            </View>
+            {/* 
 
             <View style={[styles.line4, { marginTop: 10 }]}>
                 <TouchableOpacity onPress={decrementQuantity} style={styles.button}>
@@ -56,70 +48,57 @@ export default function Slide() {
                     title="incluir"
                     buttonStyle={{ backgroundColor: '#750310', width: 140, height: 30, marginTop: 10 }}
                     titleStyle={{ color: 'white', fontSize: 15 }}
-                /* onPress={() => navigation.navigate('Tela')} */
+                /* onPress={() => navigation.navigate('Tela')} 
                 />
-            </View>
-        </View>
+            </View> */}
+        </TouchableOpacity>
     );
 }
 
 const styles = StyleSheet.create({
     slide: {
-        width: 180,
-        height: 270,
-        backgroundColor: '#00070A',
+        flex: 1,
         marginHorizontal: 7,
-        borderRadius: 5,
-        borderWidth: 1,
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 1,
-        },
-        shadowOpacity: 0.4,
-        shadowRadius: 10,
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderBottomColor: '#E6E6E6',
+        justifyContent: 'space-between',
+    },
+    colum1: {
+        flex:2,
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        marginHorizontal: 25,
+    },
+    colum2: {
+        flex:1,
+        flexDirection: 'column',
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        
     },
     itemImage: {
         width: 90,
         height: 90,
-        bottom: 0,
-    },
-    heartButton: {
-        position: 'absolute',
-        top: 0,
-        right: 0,
-    },
-    line1: {
-        width: '100%',
-        height: 110,
-        flexDirection: 'row',
-        alignItems: 'flex-end',
-        justifyContent: 'center',
-    },
-    line2: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 10,
-    },
-    line4: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    buttonContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
+        borderRadius: 7,
+        marginVertical: 10,
+        marginRight: 7,
+
     },
     title2: {
-        color: '#E1E1E6',
-        fontSize: 17,
+        fontSize: 14,
         letterSpacing: 1.2,
-        fontWeight: '450',
+        fontWeight: '550',
     },
-    title3: {
-        color: '#E1E1E6',
+    nomeItem: {
+        color: '#0D0D0D',
         fontSize: 15,
+        fontWeight: '600',
+    },
+    descricao:{
+        color: '#7C7C8A',
+        fontSize: 12,
         fontWeight: '400',
     },
     icon: {
