9da388dcdf147a7e570148e9c8bf61696a28c4de
Anunciada <jamilly.d.castro@accenture.com>
2023-10-08 16:52:29 -0300

    ajuste concluído tela de menu

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Menu'>
+      <Stack.Navigator initialRouteName='Login'>
         <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
         <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
         <Stack.Screen name="RecuperaSenha" component={RecuperaSenha} options={{headerShown: false}} />
--- views/menu.js
+++ views/menu.js
@@ -13,7 +13,6 @@ export default function Menu({ navigation }) {
           <Text style={styles.infoNameUser}>{nomeUser},</Text>
           <Text style={styles.infoText}>o que você quer fazer agora?</Text>
         </View>
-
         <View style={styles.options}>
           <TouchableOpacity style={styles.option} onPress={() => navigation.navigate('FormConta')}>
             <Text style={styles.optionText}>Configuração da conta</Text>
@@ -25,8 +24,6 @@ export default function Menu({ navigation }) {
             <Text style={styles.optionText}>Sair</Text>
           </TouchableOpacity>
         </View>
-
-
       </View>
       <Footer />
     </View>
@@ -54,43 +51,33 @@ const styles = StyleSheet.create({
     marginBottom: 23,
     marginTop: 20,
   },
-  infoNameUser:{
+  infoNameUser: {
     color: '#0D0D0D',
     fontSize: 20,
     fontWeight: '600',
   },
-  infoText:{
+  infoText: {
     color: '#FF9431',
     fontSize: 14,
     fontWeight: '500',
   },
-  
   options: {
     width: '94%',
     flexDirection: 'column',
     alignItems: 'flex-start',
-    backgroundColor: '#b8b8b8',
-    marginTop: 40,
+    marginTop: 20,
   },
-  input: {
+  option: {
     width: '100%',
-    height: 40,
-    paddingHorizontal: 10,
-    placeholderTextColor: '#7C7C8A',
-    color: '#7C7C8A',
-  },
-  divider: {
-    width: '88%',
+    height: 50,
     borderBottomWidth: 1,
-    borderBottomColor: '#192227',
-    paddingHorizontal: 16,
-    paddingVertical: 8,
-    marginTop: 40,
+    borderBottomColor: '#C4C4CC',
+    justifyContent: 'center',
   },
-  exit: {
-    color: '#C4C4CC',
-    fontSize: 20,
-    letterSpacing: 1.2,
-    fontWeight: '450',
-  },
+  optionText: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#4D585E',
+    paddingLeft: 5,
+  }
 });
