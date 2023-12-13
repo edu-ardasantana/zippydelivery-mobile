f2cbcc51aaf90a0e2d117454808ce2bc836c0405
João Victor Peregrino Novaes <jvpn@discente.ifpe.edu.br>
2023-12-12 21:12:40 -0300

    cupom desconto

--- views/sacola.js
+++ views/sacola.js
@@ -5,7 +5,8 @@ import { Button } from 'react-native-elements';
 import ItemSacola from './component/itemSacola';
 import React, { useEffect, useState } from 'react';
 import { Link, useIsFocused } from '@react-navigation/native';
-import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View, TextInput } from 'react-native';
+import { text } from 'body-parser';
 
 export default function Sacola({ navigation }) {
 
@@ -17,18 +18,36 @@ export default function Sacola({ navigation }) {
     const userId = parseInt(localStorage.getItem('id'));
     const isFocused = useIsFocused();
 
+    const [getCupom, setCupom] = useState('');
+    const [cupomInfo, setCupomInfo] = useState(null);
+
     const { cart, setCart } = useMyContext();
 
     const limparSacola = () => {
         setCart([]);
     };
 
+    function aplicarCupom() {
+        axios.get('http://localhost:8080/api/cupom/codigo/' + getCupom)
+            .then(function (response) {
+                if (response.data !== null && response.data != []) {
+                    setCupomInfo(response.data);
+                    console.log("Percentual de Desconto:", response.data.percentualDesconto);
+                }
+                else {
+                    console.log("Cupom inválido ou código inexistente.");
+                }
+            })
+            .catch(function (error) {
+                console.log("Algo deu errado.");
+            });
+    }
+
 
     useEffect(() => {
         axios.get(`http://localhost:8080/api/cliente/user/${userId}`)
             .then(function (response) {
                 setEndereco(response.data)
-
             })
             .catch(function (error) {
                 console.log(error)
@@ -83,8 +102,8 @@ export default function Sacola({ navigation }) {
                 </TouchableOpacity>
             </View>
 
-            <Text style={styles.enderecoTitle}>Entregar no endereço</Text> 
-           
+            <Text style={styles.enderecoTitle}>Entregar no endereço</Text>
+
             {enderecoCompleto === null ?
                 <View style={styles.semEndereco}>
                     <TouchableOpacity onPress={() => navigation.navigate('FormEndereco', { origin: 'Sacola' })}>
@@ -111,7 +130,7 @@ export default function Sacola({ navigation }) {
             </View>
             {cart.length === 0 && (
                 <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', alignContent: 'center' }}>
-                    <Image style={{ height: 200, width: 200, alignSelf: 'center' }} source={ { uri: 'https://api.iconify.design/material-symbols:shopping-cart-outline-sharp.svg?color=%23e6e6e6', }}></Image>
+                    <Image style={{ height: 200, width: 200, alignSelf: 'center' }} source={{ uri: 'https://api.iconify.design/material-symbols:shopping-cart-outline-sharp.svg?color=%23e6e6e6', }}></Image>
                     <Text style={{ padding: 10, textAlign: 'center', fontWeight: '500', color: '#4D585E' }}>O seu carrinho está vazio!</Text>
                 </View>)}
             {/*<Text style={{flex:1, justifyContent:'center', textAlign:'center', textAlignVertical:'center'}}>O carrinho está vazio!</Text>*/}
@@ -123,18 +142,59 @@ export default function Sacola({ navigation }) {
                 />
             )}
             {cart.length > 0 && (
-                <Text style={{ paddingHorizontal: 20, fontWeight: 600, marginVertical: 20 }}>Taxa de entrega: {cart[0].categoria.empresa.taxaFrete.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</Text>
+                <Text style={{ paddingHorizontal: 20, fontWeight: 600, marginTop: 20 }}>Taxa de entrega: {cart[0].categoria.empresa.taxaFrete.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</Text>
+            )}
+            {cart.length > 0 && (
+                <View style={styles.espacoCupom}>
+                    <TextInput
+                        style={styles.inputCupom}
+                        placeholder='O código do cupom vai aqui.'
+                        placeholderTextColor='#C4C4CC'
+                        onChangeText={text => { setCupom(text); console.log(text); }}
+                    />
+
+                    <Button
+                        buttonStyle={styles.buttonCupom}
+                        title={"Aplicar"}
+                        onPress={() => aplicarCupom()}
+                        disabled={enderecoCompleto === null && cart.length > 0}
+                    />
+                </View>
             )}
             <br />
             <View style={styles.footerContainer}>
                 {cart.length > 0 && (
                     <View style={styles.footer2}>
-                        <Text style={styles.footerText}>Total com a entrega</Text>
-                        <Text style={styles.preco}>{formatarMoeda((cartTotal + cart[0].categoria.empresa.taxaFrete))}</Text>
+                        <Text style={styles.footerText}>Total com a entrega:</Text>
+                        {cupomInfo ? ( // Verifica se cupomInfo tem dados
+                            <>
+                                <View style={{ flexDirection: "column" }}>
+                                    <Text style={styles.preco}>
+                                        {formatarMoeda(
+                                            (cartTotal * (1 - cupomInfo.percentualDesconto / 100) +
+                                                cart[0].categoria.empresa.taxaFrete)
+                                        )}
+                                    </Text>
+                                    <Text style={styles.sobreCupom}>
+                                        Desconto do cupom aplicado: {cupomInfo.percentualDesconto}%
+                                    </Text>
+                                </View>
+                            </>
+                        ) : (
+                            <Text style={styles.preco}>
+                                {formatarMoeda(cartTotal + cart[0].categoria.empresa.taxaFrete)}
+                            </Text>
+                        )}
                     </View>
                 )}
                 {(enderecoCompleto === null && cart.length > 0) && (
-                    <Text style={{ paddingTop: 20, alignSelf: 'center', fontWeight: 'bold' }}>Para continuar, informe um <Link to='/FormEndereco' state={{ origin: 'Sacola' }} style={{ color: '#FF9431' }} >endereço</Link> para entrega</Text>
+                    <Text style={{ paddingTop: 20, alignSelf: 'center', fontWeight: 'bold' }}>
+                        Para continuar, informe um{' '}
+                        <Link to="/FormEndereco" state={{ origin: 'Sacola' }} style={{ color: '#FF9431' }}>
+                            endereço
+                        </Link>{' '}
+                        para entrega
+                    </Text>
                 )}
                 <Button
                     buttonStyle={styles.button}
@@ -231,10 +291,11 @@ const styles = StyleSheet.create({
     },
     footer2: {
         flexDirection: 'row',
-        justifyContent: 'space-around',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
     },
     footerText: {
-        paddingRight: 25
+        paddingRight: 25,
     },
     preco: {
         fontWeight: 'bold',
@@ -247,4 +308,31 @@ const styles = StyleSheet.create({
         width: 300,
         borderRadius: 5
     },
+    buttonCupom: {
+        backgroundColor: '#44AB65',
+        height: 30,
+        width: 90,
+        borderRadius: 5,
+    },
+    inputCupom: {
+        height: 30,
+        width: 220,
+        placeholderTextColor: '#ABABAB',
+        color: '#ABABAB',
+        borderColor: '#E6E6E6',
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingLeft: 6,
+    },
+    espacoCupom: {
+        flexDirection: 'row',
+        marginHorizontal: 16,
+        marginTop: 8,
+        alignItems: 'center',
+        justifyContent: "space-between",
+    },
+    sobreCupom: {
+        fontSize: 12,
+        color: '#44AB65',
+    }
 });
